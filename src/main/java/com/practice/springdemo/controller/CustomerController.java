60e2e03d72941fc574e26822db3d44a03dcb6e40
mint93 <mint5999@gmail.com>
2017-12-12 21:18:20 +0100

    Working configuration

---
+++ src/main/java/com/practice/springdemo/controller/CustomerController.java
@@ -0,0 +1,18 @@
+package com.practice.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	public CustomerController() {
+	}
+	
+	@RequestMapping("/list")
+	public String listCustomers(Model model) {
+		return "list-customers";
+	}
+}
